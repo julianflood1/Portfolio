954ed2f988a6b4c39c711fa432953fb73d12315b
Julian Flood <Julian@Julians-MacBook-Pro.local>
2017-06-16 17:01:04 -0700

    fix parallax, work on nav link

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,21 @@
 
-var boxes = $('[data-scroll-speed]'),
-$window = $(window);
-$window.on('scroll', function(){
-  var scrollTop = $window.scrollTop();
-  boxes.each(function(){
-    var $this = $(this),
-        scrollspeed = parseInt($this.data('scroll-speed')),
-        val = - scrollTop / scrollspeed;
-    $this.css('transform', 'translateY(' + val + 'px)');
+    // Parralax Scrolling
+    $(document).ready(function() {
+
+  var boxes = $('[data-scroll-speed]'),
+  $window = $(window);
+  $window.on('scroll', function(){
+    var scrollTop = $window.scrollTop();
+    boxes.each(function(){
+      var $this = $(this),
+          scrollspeed = parseInt($this.data('scroll-speed')),
+          val = - scrollTop / scrollspeed;
+      $this.css('transform', 'translateY(' + val + 'px)');
+    });
   });
 
-$(".experience-link").click(function(){
-  $("body, html").stop()
-  $("body, html").animate({ scrollTop: $(".experience").offset().top - 40}, "slow");
-});
-
-  $('.skills-link').click(function() {
-    $('html, body').animate({ scrollTop: $(".skills").offset().top }, "slow");
-  });
-
-  $('.contact-link').click(function() {
-    $('html, body').animate({ scrollTop: $(".contact").offset().top }, "slow");
+  $(".experience-link").click(function(){
+    $("body, html").stop()
+    $("body, html").animate({ scrollTop: $(".experience").offset().top - 40}, "slow");
   });
 });
