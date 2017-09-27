0f63ed9dfd2e7df6c8786ee5bcfe7b0d93f73f8f
jf <julianflood@gmail.com>
2017-09-27 12:02:32 -0700

    add modals and assets for designs section, still need to add drawn images if available

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 
+
     // Parralax Scrolling
     $(document).ready(function() {
 
@@ -42,4 +43,80 @@
     }
 
   });
+
+  // Get the modal
+  var modal = document.getElementById('myModal');
+  var modal2 = document.getElementById('myModal2');
+  var modal3 = document.getElementById('myModal3');
+  var modal4 = document.getElementById('myModal4');
+  var modal5 = document.getElementById('myModal5');
+
+
+  // Get the button that opens the modal
+  var trigger1 = document.getElementById("trigger1");
+  var trigger2 = document.getElementById("trigger2");
+  var trigger3 = document.getElementById("trigger3");
+  var trigger4 = document.getElementById("trigger4");
+  var trigger5 = document.getElementById("trigger5");
+
+
+  // Get the <span> element that closes the modal
+  var span = document.getElementsByClassName("close")[0];
+  var span2 = document.getElementsByClassName("close")[1];
+  var span3 = document.getElementsByClassName("close")[2];
+  var span4 = document.getElementsByClassName("close")[3];
+  var span5 = document.getElementsByClassName("close")[4];
+
+
+
+  // When the user clicks on the button, open the modal
+  trigger1.onclick = function() {
+      modal.style.display = "block";
+  }
+  trigger2.onclick = function() {
+      modal2.style.display = "block";
+  }
+  trigger3.onclick = function() {
+      modal3.style.display = "block";
+  }
+  trigger4.onclick = function() {
+      modal4.style.display = "block";
+  }
+  trigger5.onclick = function() {
+      modal5.style.display = "block";
+  }
+
+  // When the user clicks on <span> (x), close the modal
+  span.onclick = function() {
+      modal.style.display = "none";
+  }
+  span2.onclick = function() {
+      modal2.style.display = "none";
+  }
+  span3.onclick = function() {
+      modal3.style.display = "none";
+  }
+  span4.onclick = function() {
+      modal4.style.display = "none";
+  }
+  span5.onclick = function() {
+      modal5.style.display = "none";
+  }
+
+  // When the user clicks anywhere outside of the modal, close it
+  window.onclick = function(event) {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      } else if (event.target == modal2) {
+        modal2.style.display = "none";
+      } else if (event.target == modal3) {
+        modal3.style.display = "none";
+      } else if (event.target == modal4) {
+        modal4.style.display = "none";
+      } else if (event.target == modal5) {
+        modal5.style.display = "none";
+      } 
+  }
+
+
 });
