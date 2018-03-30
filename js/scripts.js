3a101fface2ead2310a5a1c39a2075af5d360d25
jf <julianflood@gmail.com>
2018-03-30 12:54:14 -0700

    test

--- js/scripts.js
+++ js/scripts.js
@@ -145,7 +145,7 @@ $(document).ready(function() {
       modal9.style.display = "none";
   }
 
-  // When the user clicks anywhere outside of the modal, close it
+  // When the user clicks anywhere outside of the modal, close modal
   window.onclick = function(event) {
       if (event.target == modal) {
           modal.style.display = "none";
